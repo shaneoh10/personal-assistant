bd3dd2b5f65ffd85b068492684a6459020cbfba0
Shane O Hanlon <ShaneOHanlon@laptop.local>
2022-01-14 11:14:19 +0000

    Set up speech recognition for user input

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pyttsx3
+import speech_recognition as sr
+from random import choice
 from decouple import config
+from datetime import datetime
+from utils import opening_text
 
 USERNAME = config('USER')
 BOTNAME = config('BOTNAME')
@@ -17,8 +21,49 @@ engine.setProperty('volume', 1.0)
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
+
 def speak(text):
     """Convert text passed to function as speech"""
 
     engine.say(text)
     engine.runAndWait()
+
+
+def greet_user():
+    """Greet user according to current time"""
+
+    hour = datetime.now().hour
+    if (hour >= 6) and (hour < 12):
+        speak(f"Good Morning {USERNAME}")
+    elif (hour >= 12) and (hour < 16):
+        speak(f"Good Afternoon {USERNAME}")
+    elif (hour >= 16) and (hour < 19):
+        speak(f"Good Evening {USERNAME}")
+    speak(f"I am {BOTNAME}. How may I assist you?")
+
+
+def take_user_input():
+    """Takes user speech input and converts it to text"""
+
+    r = sr.Recognizer()
+    with sr.Microphone as source:
+        print('Listening....')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing....')
+        query = r.recognize_google(audio, language='en-us')
+        if 'exit' not in query or 'stop' in query:
+            speak(choice(opening_text))
+        else:
+            hour = datetime.now().hour
+            if hour >= 21 and hour < 6:
+                speak("Good night sir, take care!")
+            else:
+                speak("Have a good day sir!")
+            exit()
+    except Exception:
+        speak("Sorry, I could not understand. Could you please say that again?")
+        query = "None"
+    return query
