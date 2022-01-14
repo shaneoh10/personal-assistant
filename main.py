941eff0acd322e240d4f5156b169f9bd853ef774
Shane O Hanlon <ShaneOHanlon@laptop.local>
2022-01-14 10:56:03 +0000

    Set up text to speech engine

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("WORKING")
+import pyttsx3
+from decouple import config
+
+USERNAME = config('USER')
+BOTNAME = config('BOTNAME')
+
+
+engine = pyttsx3.init('sapi5')
+
+# Set Rate
+engine.setProperty('rate', 190)
+
+# Set Volume
+engine.setProperty('volume', 1.0)
+
+# Set Voice
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def speak(text):
+    """Convert text passed to function as speech"""
+
+    engine.say(text)
+    engine.runAndWait()
